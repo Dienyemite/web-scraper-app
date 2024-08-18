be0c8fdc4b418ebc6a57a505a6e123146841e36a
AlexandruSoroiu <alexandru.soroiu@wagner.edu>
2024-08-18 22:10:02 +0300

    updated app.js

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const app = express();
-const PORT = process.env.PORT || 3000;
+// const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 
 // MongoDB connection
 mongoose.connect('mongodb://localhost:27017/')
